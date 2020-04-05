61dfb0903ec239913583b2fda41e55ef73490dc6
praj1663 <praj1663@gmail.com>
2020-04-05 11:01:22 +0530

    1)Task File Upload Code Implemented Need to set file name to file control on getting edit details from DB. 2)subtask Checkboxes added need to udate Status as per check and uncheck

--- 365_Portal/Code/BL/TaskBL.cs
+++ 365_Portal/Code/BL/TaskBL.cs
@@ -5,6 +5,7 @@ using System.Web;
 using _365_Portal.Models;
 using _365_Portal.Code.DAL;
 using System.Data;
+using System.IO;
 
 namespace _365_Portal.Code.BL
 {
@@ -17,16 +18,39 @@ namespace _365_Portal.Code.BL
 
         public static DataSet TaskCRUD(TaskManagement task)
         {
-            DataSet ds = new DataSet();
+            DataSet dsresult = new DataSet();
             try
             {
-                ds = TaskDAL.TaskCRUD(task);
+                if (!string.IsNullOrEmpty(task.t_FileIds))
+                {
+                    string userProfilePicBase64 = string.Empty;
+
+
+                    var files = task.t_FileIds.Split(new string[] { "," }, StringSplitOptions.None);
+
+                    if (files.Count() == 1)
+                        userProfilePicBase64 = files[0];
+                    else
+                        userProfilePicBase64 = files[1];
+                    byte[] imageBytes = Convert.FromBase64String(userProfilePicBase64);
+
+                    string fileName = task.t_UserId + "_" + Guid.NewGuid() + "." + Utility.GetFileExtension(userProfilePicBase64);
+                    string filePath = HttpContext.Current.Server.MapPath("~/Files/Task/" + fileName);
+                    File.WriteAllBytes(filePath, imageBytes);
+
+                    DataSet dsFile = UserBL.CreateFile(fileName, HttpContext.Current.Server.MapPath("~/Files/Task/"), false, "Task");
+                    if (dsFile.Tables.Count > 0 && dsFile.Tables[0].Rows.Count > 0)
+                    {
+                        task.t_FileIds = dsFile.Tables[0].Rows[0]["UniqueID"].ToString().Trim();
+                    }
+                }
+                dsresult = TaskDAL.TaskCRUD(task);
             }
             catch (Exception ex)
             {
                 Log(ex, System.Reflection.MethodBase.GetCurrentMethod().Name);
             }
-            return ds;
+            return dsresult;
         }
 
     }
