c7e53e04362364c815c5a40ab2ec4166d028f32f
Imtiyaz Khan <Imtiyazkhan.xbiz@outlook.com>
2020-04-12 21:04:12 +0530

    default page

--- 365_Portal/t/default.aspx.designer.cs
+++ 365_Portal/t/default.aspx.designer.cs
@@ -15,13 +15,13 @@ namespace Life
     {
 
         /// <summary>
-        /// dvUserName1 control.
+        /// dvUserName control.
         /// </summary>
         /// <remarks>
         /// Auto-generated field.
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
-        protected global::System.Web.UI.HtmlControls.HtmlGenericControl dvUserName1;
+        protected global::System.Web.UI.HtmlControls.HtmlGenericControl dvUserName;
 
         /// <summary>
         /// dvAdminTasks control.
