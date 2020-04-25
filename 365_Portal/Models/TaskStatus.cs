850bc9dc2c2b93a5139772c1d42fb75e67671561
praj1663 <praj1663@gmail.com>
2020-04-25 19:20:24 +0530

    added task status file

---
+++ 365_Portal/Models/TaskStatus.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace _365_Portal.Models
+{
+    public class TaskStatus
+    {
+        public int p_Action { get; set; }
+        public int p_CompID { get; set; }
+        public string p_StatusName { get; set; }
+        public int p_SrNo { get; set; }
+        public int p_UserId { get; set; }
+        public int p_ProjectID { get; set; }
+    }
+}
